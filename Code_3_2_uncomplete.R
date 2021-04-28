5fac5bebc3c9509cb39cf80e95d075880adf0e6b
scottvdv <36049405+scottvdv@users.noreply.github.com>
2021-04-27 14:09:00 -1000

    Add files via upload

---
+++ Code_3_2_uncomplete.R
@@ -0,0 +1,23 @@
+#### Chapter 3
+#### computation of sequence A and B
+
+A<-c(45,32,12,23,26,27,39)
+B<-c(1040,1027,1007,1018,1021,1022,1034)
+
+
+## Code 3.2
+
+# specifying r, t, and cutoff
+r<- 
+t<-
+cutoff<-
+
+# Equation 3.57
+
+prob<- function(k,t) {
+  ((rt)/k)(e^(-rt)*(rt)^(k-1)/(k-1)!
+}
+prob(0,t)<- e^(-rt)
+
+
+### Psudocode for multinomial
